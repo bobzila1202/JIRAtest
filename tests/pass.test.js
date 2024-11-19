e517394e6bbbb6eb0f829b80077d81675704b5c3
Balram SINGH <balram.singh@asn.com>
2024-11-19 11:52:11 +0000

    DP-5 ok

---
+++ tests/pass.test.js
@@ -0,0 +1,69 @@
+import { test, expect } from '@playwright/test';
+
+ 
+
+test('passing test', async ({ page }) => {
+
+  await page.goto('https://www.google.com/', { waitUntil: 'load', timeout: 30000 });
+
+ 
+
+  await page.getByRole('button', { name: 'Reject all' }).click();
+
+  await page.getByLabel('Search', { exact: true }).click();
+
+  await page.getByLabel('Search', { exact: true }).fill('alcatel submarine networks');
+
+ 
+
+  await page.goto('https://www.google.com/search?q=alcatel+submarine+networks', { waitUntil: 'load', timeout: 30000 });
+
+ 
+
+  await page.locator('#rso').getByRole('link', { name: 'Alcatel Submarine Networks: Home Alcatel Submarine Networks https://www.asn.com' }).click();
+
+ 
+
+  await page.waitForTimeout(5000);
+
+});
+
+ 
+
+ 
+
+(Failing Test)
+
+ 
+
+import { test, expect } from '@playwright/test';
+
+ 
+
+test('failing test', async ({ page }) => {
+
+  await page.goto('https://www.google.com/', { waitUntil: 'load', timeout: 30000 });
+
+ 
+
+  await page.getByRole('button', { name: 'Reject all button does not exist' }).click();
+
+ 
+
+  await page.getByLabel('Search', { exact: true }).click();
+
+  await page.getByLabel('Search', { exact: true }).fill('alcatel submarine networks');
+
+ 
+
+  await page.goto('https://www.google.com/search?q=alcatel+submarine+networks', { waitUntil: 'load', timeout: 30000 });
+
+ 
+
+  await page.locator('#rso').getByRole('link', { name: 'Nonexistent Link' }).click();
+
+ 
+
+  await page.waitForTimeout(5000);
+
+});
