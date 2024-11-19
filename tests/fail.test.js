bc68b32761d954debe6f97c8b651e42e7bac0c77
Balram SINGH <balram.singh@asn.com>
2024-11-19 11:53:58 +0000

    DP-5 ok

--- tests/fail.test.js
+++ tests/fail.test.js
@@ -2,13 +2,15 @@ import { test, expect } from '@playwright/test';
 
  
 
-test('passing test', async ({ page }) => {
+test('failing test', async ({ page }) => {
 
   await page.goto('https://www.google.com/', { waitUntil: 'load', timeout: 30000 });
 
  
 
-  await page.getByRole('button', { name: 'Reject all' }).click();
+  await page.getByRole('button', { name: 'Reject all button does not exist' }).click();
+
+ 
 
   await page.getByLabel('Search', { exact: true }).click();
 
@@ -20,13 +22,10 @@ test('passing test', async ({ page }) => {
 
  
 
-  await page.locator('#rso').getByRole('link', { name: 'Alcatel Submarine Networks: Home Alcatel Submarine Networks https://www.asn.com' }).click();
+  await page.locator('#rso').getByRole('link', { name: 'Nonexistent Link' }).click();
 
  
 
   await page.waitForTimeout(5000);
 
 });
-
-
- 
