0bbc933ae93223aeefa6982785c7fbd8559dfd62
Balram SINGH <balram.singh@asn.com>
2024-11-19 13:52:34 +0000

    DP-5 ok

--- tests/pass.test.js
+++ tests/pass.test.js
@@ -4,29 +4,12 @@ import { test, expect } from '@playwright/test';
 
 test('passing test', async ({ page }) => {
 
-  await page.goto('https://www.google.com/', { waitUntil: 'load', timeout: 30000 });
-
  
 
-  await page.getByRole('button', { name: 'Reject all' }).click();
-
-  await page.getByLabel('Search', { exact: true }).click();
-
-  await page.getByLabel('Search', { exact: true }).fill('alcatel submarine networks');
-
- 
-
-  await page.goto('https://www.google.com/search?q=alcatel+submarine+networks', { waitUntil: 'load', timeout: 30000 });
-
- 
-
-  await page.locator('#rso').getByRole('link', { name: 'Alcatel Submarine Networks: Home Alcatel Submarine Networks https://www.asn.com' }).click();
+  await page.goto('https://www.asn.com/', { waitUntil: 'load', timeout: 30000 });
 
  
 
   await page.waitForTimeout(5000);
 
 });
-
-
- 
